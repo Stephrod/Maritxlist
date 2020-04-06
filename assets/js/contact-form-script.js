5b72bfb60b2c45f353cf89837c85b3fb1c607416
Stephanie <StephanieRodriguez@Stephanies-MBP.lan>
2020-04-06 10:58:16 -0500

    url change in contact-form

--- assets/js/contact-form-script.js
+++ assets/js/contact-form-script.js
@@ -27,7 +27,7 @@
 
         $.ajax({
             type: "POST",
-            url: "assets/php/form-process.php",
+            url: "stephrodwebdev@gmail.com",
             data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
             success : function(text){
                 if (text == "success"){
